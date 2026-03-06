87cf150c1dea20147f05f087ed52a6ec4b7897e8
Max_dev <maxime2008.lucas@gmail.com>
2026-03-07 00:42:20 +0100

    get the streamer working
    work in progress streamer is on but thinking appear in the web page

--- Frontside/Main_js.js
+++ Frontside/Main_js.js
@@ -1,14 +1,35 @@
 async function SendChatBot() {
  var inputQuestion = document.getElementById("inputQuestion").value;
+ let Final_text = ""
+ let BufferThink = ""
 
     // Send the question to the backend
-    var response = await fetch("http://localhost:5000/API", {
+    var response = await fetch("http://localhost:5000/API/Streamer", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({ message: inputQuestion })
     });
+
     document.getElementById("inputQuestion").value = ""; // Clear the input field after sending
-    var IA_response = await response.json()
-    document.getElementById("ChatContainer").innerHTML +=  "<br>" + IA_response.reponse
+    
+    document.getElementById("ChatContainer").innerHTML += "<br>" + "--------------------------" + "<br>"
+    const reader = response.body.getReader()
+    const decoder = new TextDecoder()
+    while (true) {
+        const { done, value } = await reader.read()
+        if (done) break
+    
+            const chunk = decoder.decode(value)
+            //BufferThink += chunk
+            //if BufferThink == "<think/>":
+            
+            Final_text += chunk
+            console.log(Final_text)
+            document.getElementById("ChatContainer").innerHTML = Final_text
+
+    }
+    
+
+
 
 }
--- Server_side/Main_AI_text_return.py
+++ Server_side/Main_AI_text_return.py
@@ -1,8 +1,9 @@
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, TextIteratorStreamer
 import torch
+from threading import Thread
 
 app = Flask(__name__)
 CORS(app)
@@ -11,20 +12,20 @@ CORS(app)
 #Token management
 TokenAvailable = 100
 TokenUsed = 0
-
+AI_Role = "You are a helpful assistant."
 # Load the model and tokenizer
-model_id = "Qwen/Qwen3-0.6B"
+model_id = "Qwen/Qwen3-0.6B" #get a fast model to test
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float32)
+model = AutoModelForCausalLM.from_pretrained(model_id, dtype=torch.float16)
 
-
+#One chunk system
 @app.route('/API', methods=['POST'])
 def response_send():
     #question input
     asked_question = request.json["message"]
     #Create the message
     messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "system", "content": AI_Role},
         {"role": "user", "content": asked_question}
     ]
 
@@ -45,6 +46,34 @@ def response_send():
     print("Request Complete")
     response = response.split("</think>")[-1].strip()
     return jsonify({"reponse": response})
+
+@app.route("/API/Streamer", methods =["POST"])
+def Stream_Response():
+     #question input
+    asked_question = request.json["message"]
+    #Create the message
+    messages = [
+        {"role": "system", "content": AI_Role},
+        {"role": "user", "content": asked_question}
+    ]
+    
+    Input_text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    TokenizeInput = tokenizer(Input_text, return_tensors="pt")
+    streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=False)
+
+    thread = Thread(target=model.generate, kwargs={
+        **TokenizeInput,
+        "max_new_tokens": 700,
+        "temperature": 0.7,
+        "do_sample": True,
+        "streamer": streamer
+    })
+    thread.start()
+    def generer():
+        for token in streamer:
+            yield f" {token}\n\n"
+
+    return Response(generer(), mimetype="text/event-stream")
 
 
     #Launch Server
